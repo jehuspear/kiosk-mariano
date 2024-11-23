bb5f358ad938f70394da540c44028cb9bbc7ca8b
Jehu Galvez <galvezjf@students.nu-fairview.edu.ph>
2024-11-23 08:42:48 +0800

    updated home page

--- customer/home.php
+++ customer/home.php
@@ -8,18 +8,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mariano Cafe - Home</title>
-    <link rel="stylesheet" href="/css/styles.css">
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
     <div class="home-page">
         <div class="logo-container">
-            <img src="/resources/images/logo.png" alt="Mariano Cafe Logo" class="logo">
+            <img src="resources/images/logo.png" alt="Mariano Cafe Logo" class="logo">
             <p class="tagline">Where Good Coffee Starts</p>
         </div>
         <div class="tap-to-start">
             <button onclick="startApp()">Tap To Start</button>
         </div>
     </div>
-    <script src="script.js"></script>
+    <script src="javascript/script.js"></script>
 </body>
 </html>

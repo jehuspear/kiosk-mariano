706bd9d42a0d06288febab270e1b6518b91ab88b
Jehu Galvez <galvezjf@students.nu-fairview.edu.ph>
2025-01-11 02:46:57 +0800

    fix auto-refresh at preparing-orders.php page

--- admin-staff/menuscreen.php
+++ admin-staff/menuscreen.php
@@ -2,10 +2,10 @@
 session_start();
 
 // Check if user is not logged in
-// if(!isset($_SESSION["user_id"])) {
-//     header("Location: login.php");
-//     exit();
-// }
+if(!isset($_SESSION["user_id"])) {
+    header("Location: login.php");
+    exit();
+}
 
 include 'database_admin.php';
 
